e7ae3cbc51621284c5f8040a923331dd4380a4f1
Kevin Johnson <john2003.kjj@gmail.com>
2024-12-01 16:49:19 -0600

    now uses api key to attain level two authentication

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-from py_clob_client.client import ClobClient
+from py_clob_client.client import ClobClient, ApiCreds
 from dotenv import dotenv_values
 
 config = dotenv_values(".env")
 
 endpoint: str = "https://clob.polymarket.com/"
+creds = ApiCreds(config["API_KEY"], config["API_SECRET"], config["API_PASSPHRASE"])
 key: str = config["POLYMARKET_PRIVATE_KEY"]
 chain_id: int = 137
 
-client: ClobClient = ClobClient(endpoint, key=key, chain_id=chain_id)
+client: ClobClient = ClobClient(endpoint, chain_id=chain_id, key=key, creds=creds)
 
-creds = client.derive_api_key()
+marketsData = client.get_markets(next_cursor="")
 
-print(creds)
+markets = marketsData["data"]
+
+questions = []
+
+for market in markets:
+    questions.append(market["question"])
+
+print(questions)
